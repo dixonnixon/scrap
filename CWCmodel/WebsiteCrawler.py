520227b7f33a87a51db8f010e77862ba74c88244
dixonnixon <drenb@ukr.net>
2024-02-15 07:50:17 +0000

    dynamyccontent

--- CWCmodel/WebsiteCrawler.py
+++ CWCmodel/WebsiteCrawler.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 from content import Content
 from website import Website
+from playwright.sync_api import sync_playwright
+from pathlib import Path
+
 
 '''
     use single string CSS selector for each piece of info
@@ -16,10 +19,28 @@ from website import Website
 class Crawler:
     def getPage(self, url): #pretty cool) 
         try:
-            req = requests.get(url)
+            # req = requests.get(url)
+            with sync_playwright() as p:
+                browser = p.chromium.launch()
+                page = browser.new_page()
+                page.goto(url)
+
+                # Wait for the page to load fully
+                page.wait_for_timeout(1000)
+
+                # Now you can access the full content of the page, including any dynamic content loaded by JavaScript
+                html = page.content()
+
+                with open(str(Path.cwd()) + "/CWCmodel/data/dyn.txt", "w") as file1:
+                    # Writing data to a file
+                    file1.write(html)
+
+                # print(html)
+
+                browser.close()
         except requests.exceptions.RequestException:
             return None
-        return BeautifulSoup(req.text, 'html.parser')
+        return BeautifulSoup(html, 'html.parser')
     
     def safeGet(self, pageObj, selector): #almost good
         """
@@ -54,9 +75,9 @@ class Crawler:
         """
         bs = self.getPage(site.searchUrl + topic)
         searchResults = bs.select(site.resultListing)
-        print(site.searchUrl + topic, site.resultListing, len(searchResults))
+        print( len(searchResults))
         for result in searchResults:
-            print(type(result),  site.resultUrl, result.select(site.resultUrl))
+            print(type(result),   site.resultUrl, '\n\n\n',result.select(site.resultUrl)[0])
             url = result.select(site.resultUrl)[0].attrs["href"]
             # Check to see whether it's a relative or an absolute URL
             if(site.absoluteUrl):
@@ -86,8 +107,8 @@ crawler = Crawler()
 siteData = [
     ['O\'Reilly Media', 'http://oreilly.com',
         # 'https://www.oreilly.com/search/?q=','article.product-result',
-        'https://www.oreilly.com/search/?q=','article',
-        'article', True, 'h3', 'section#product-description'],
+        'https://www.oreilly.com/search/?q=','div#search-main-content >  section',
+        'article a', True, 'h3', 'section#product-description'],
     ['Reuters', 'http://reuters.com',
         'http://www.reuters.com/search/news?blob=',
         'div.search-result-content','h3.search-result-title a',
@@ -98,6 +119,8 @@ siteData = [
         'div.post-body']
 ]
 sites = []
+# name, url, searchUrl, resultListing,
+        # resultUrl, absoluteUrl, titleTag, bodyTag
 for row in siteData:
     sites.append(Website(row[0], row[1], row[2],
         row[3], row[4], row[5], row[6], row[7]))
